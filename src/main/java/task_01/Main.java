120b56bd338dc7f6505b00b0705f200d4067ccb3
maximheit1989-arch <maxim.heit1989@gmail.com>
2025-08-29 15:07:06 +0200

    Car Info

---
+++ src/main/java/task_01/Main.java
@@ -0,0 +1,19 @@
+package task_01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car("Toyota", "red", 2005);
+        Car car2 = new Car("Mazda", "black", 2007);
+        Car car3 = new Car("Nissan", "blue", 2004);
+
+        printInfo(car1);
+        printInfo(car2);
+        printInfo(car3);
+    }
+
+    public static void printInfo(Car car){
+        System.out.println("Модель - " + car.getModel() + ", цвет - " + car.getColor() + ", год - " + car.getYear());
+    }
+}
